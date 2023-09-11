45b113253adca7b4bc82faa931a5d61b6b63e347
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-09-11 10:05:08 +0700

    fix block

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-    HOST : "aws.connect.psdb.cloud",
-    USER : "i8dtabhvzm0as48y243a",
-    PASSWORD : "pscale_pw_z7ai0A27Y09gghJQLzKy2XUVqHPn73gsDWy393vqHwX",
-    DB : "se_database",
+    HOST : process.env.HOST,
+    USER : process.env.USER,
+    PASSWORD : process.env.PASSWORD,
+    DB : process.env.DB,
 }
